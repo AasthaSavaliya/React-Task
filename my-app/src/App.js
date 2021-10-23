662449fe36a20b1c056806da2db1cd5d9a4b5d20
Aastha Savaliya <aasthasavaliya26@gmail.com>
2021-10-23 11:56:20 +0530

    React Bootstrap changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,40 +1,41 @@
 import { useState } from "react";
-import React, { Component } from 'react';
 import "./App.css";
+import { Button, Form, Row, Col, Container } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   const [text, settext] = useState("");
   const [showtext, setShowtext] = useState([]);
   
   return (
-    <div className="main">
-      <div className="left">
-      <h2 className="lbl">First name:</h2>
-        <input type="text" className="txtbox"
-          onChange={(e) => {
-            settext(e.target.value);
-          }}
-        />
+  <Container style={{backgroundColor: "#FEF5ED"}}>
+  <Row>
+    <Col>
+      <div className="left" style={{padding:"30px", margin:"70px"}}>
+      <Form>
+          <Form.Group style={{fontSize:"40px"}}>
+            <Form.Label>First Name</Form.Label>
+            <Form.Control style={{fontSize:"15px", width:"50%"}} type="text" placeholder="First Name" onChange={(e) => {settext(e.target.value); }}/>
+          </Form.Group>
+      </Form>
         <br />
-        <button type="button" onClick={() => {
+        <Button onClick={() => {
             setShowtext(showtext.concat(text));}} 
-            className="btn btn-secondary">Submit</button>
+            className="btn btn-lg btn-secondary">Submit</Button>
       </div>
-      <div className="right">
-      <div className="scrollable">
+      </Col>
+      <Col>
+      <div className="right" style={{padding:"30px", margin:"70px"}}>
+      <div>
         {showtext.map((e) => {
-          
           return <h1>{e}</h1>
-          
         })}
-        
-            </div>
-            <button type="button" onClick={() => {
-            setShowtext([]);}} 
-            className="btn btn-secondary">Clear</button>
-    </div>
-    
-</div>
+        </div>
+        <Button type="button" onClick={() => { setShowtext([]);}} style={{alignItem:"center"}} className="btn btn-lg btn-secondary">Clear</Button>
+      </div>
+    </Col>
+    </Row>
+</Container>
   );
 }
 
